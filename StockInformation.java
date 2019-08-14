11c92475422aade33b9534dd91ebfea965f9008f
JuanDiego <JuanDiego085@gmail.com>
2019-08-14 18:54:05 -0400

    made changes and added other classes

--- StockInformation.java
+++ StockInformation.java
@@ -11,48 +11,71 @@ import java.util.ArrayList;
 //this code will work for yahoo mostly 
 //https://www.nasdaq.com/symbol/ko/real-time
 
-//"EQUITY","symbol" for the stock ticker symbol use the .
 public class StockInformation {
     public String price = "No data";
-     
+    public String ticker = "No Ticker"; 
     
     public void indenfityTarget (String first, String second){
         
     }
-   
     public String readPrice (String companyName, String website){
+
+        try {
+              URL url = new URL(website); 
+              URLConnection urlConn = url.openConnection (); 
+              InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream()); 
+              BufferedReader buff = new BufferedReader(inStream); 
+              String line = buff.readLine(); 
+
+              while (line != null){            
+                   if (line.contains("root.App.main = {\"context\"")){
+                      int target = line.indexOf ("\"regularMarketPrice\":{\"raw\":"); 
+                      int decimal = line.indexOf (".", target);  
+                      int start = decimal; 
+                      while (line.charAt(start) != '\"'){
+                          start --; 
+                      }
+                      price = line.substring (start +2 , decimal+3 ); 
+                   }          
+                  line=buff.readLine(); 
+              }
+          }catch(Exception IOException){
+              System.out.println("Something is wrong"); 
+          }
+          System.out.println(price);
+          return price; 
+     }
+    public String readTicker(String companyName, String website){
         try {
             URL url = new URL(website); 
             URLConnection urlConn = url.openConnection (); 
             InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream()); 
             BufferedReader buff = new BufferedReader(inStream); 
-              
             String line = buff.readLine(); 
            
             while (line != null){            
                  if (line.contains("root.App.main = {\"context\"")){
-                    int target = line.indexOf ("\"regularMarketPrice\":{\"raw\":"); 
+                    int target = line.indexOf ("\"EQUITY\",\"symbol\"");   // ("\"regularMarketPrice\":{\"raw\":"); 
+                    
                     int decimal = line.indexOf (".", target);  
                     int start = decimal; 
                     while (line.charAt(start) != '\"'){
                         start --; 
                     }
-                    price = line.substring (start +2 , decimal+3 ); 
+                    ticker = line.substring (start +2 , decimal+3 ); 
                  }          
                 line=buff.readLine(); 
             }
-            for (int i = 0; i < price.length(); i++){
-                if (price.charAt(i) == ',' ){
+            for (int i = 0; i < ticker.length(); i++){
+                if (ticker.charAt(i) == ',' ){
                     System.out.println ("Need Change, there is a comma in this stock " + companyName); 
                 }
             }
         }catch(Exception IOException){
             System.out.println("Something is wrong"); 
         }
-        System.out.println(price);
-        return price; 
+        System.out.println(ticker);
+        return ticker; 
     }
-   // public String readTicker (){
 
-   // }
 }
--- Stocks.java
+++ Stocks.java
@@ -4,36 +4,39 @@ import java.util.ArrayList;
 public class Stocks {
     static ArrayList <String> URL = new ArrayList <String>(); 
     static ArrayList <String>  tickerSym = new ArrayList <String>(); 
+    static ArrayList <String>  newSym = new ArrayList <String>(); 
     static ArrayList <String>  stockPrice = new ArrayList <String>(); 
-    
-    String UTX = "https://ca.finance.yahoo.com/quote/UTX?p=UTX&.tsrc=fin-srch";
-    String TD = "https://ca.finance.yahoo.com/quote/TD.TO?p=TD.TO&.tsrc=fin-srch";
-    String MFC  = "https://ca.finance.yahoo.com/quote/MFC.TO/key-statistics?p=MFC.TO";
-    String TSLA = "https://ca.finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch";
+    static ReadFile f = new ReadFile ("file.txt"); 
+
 
     public void StockURLs(){
-          URL.add (UTX); 
-          URL.add (TD); 
-          URL.add(MFC); 
-          URL.add(TSLA); 
+          URL.add (f.getWebsite(3)); 
+          URL.add (f.getWebsite(0)); 
+          URL.add (f.getWebsite(2)); 
+          URL.add (f.getWebsite(1)); 
     }
     public void addTickers(){
-        tickerSym.add(UTX); 
-        tickerSym.add(TD); 
-        tickerSym.add(MFC); 
-        tickerSym.add(TSLA); 
+        tickerSym.add (f.getStockSymbol(3)); 
+        tickerSym.add (f.getStockSymbol(0)); 
+        tickerSym.add (f.getStockSymbol(2)); 
+        tickerSym.add (f.getStockSymbol(1)); 
     }
     public void getInformation(){
         StockInformation a = new StockInformation(); 
-        for (int i = 0 ; i < URL.size(); i ++)
-            stockPrice.add(a.readPrice(tickerSym.get(i), URL.get(i)));
-        System.out.println(stockPrice);
+        for (int i = 0 ; i < URL.size(); i ++){
+            newSym.add(a.readTicker(tickerSym.get(i), URL.get(i)));
+           
+        } for (int i = 0 ; i < URL.size(); i ++){
+        stockPrice.add(a.readPrice(tickerSym.get(i), URL.get(i)));}
+        System.out.println(stockPrice + " "+ newSym);
     }
     public static void main (String [] args){
         Stocks s = new Stocks(); 
+        f.openFile();
+        f.readThoughFile(); 
         s.StockURLs();
         s.addTickers();
         s.getInformation(); 
-
+        f.closeFile();
     }
 }
